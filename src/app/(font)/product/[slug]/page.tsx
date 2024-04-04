6980eb35922a7d3e7bac07e6182ceca8cb8178a7
Samba Diallo <sambadiallo@MacBook-Pro-de-Sambabhouria.local>
2024-04-04 14:55:50 +0200

    11. Load Products From DB

--- src/app/(font)/product/[slug]/page.tsx
+++ src/app/(font)/product/[slug]/page.tsx
@@ -1,14 +1,31 @@
 import AddToCart from '@/components/products/AddToCart'
+import { convertDocToObj } from '@/lib/utils'
 import data from '@/lib/data'
+import productService from '@/lib/services/productService'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function ProductDetails({
+export async function generateMetadata({
   params,
 }: {
   params: { slug: string }
 }) {
-  const product = data.products.find((x) => x.slug === params.slug)
+  const product = await productService.getBySlug(params.slug)
+  if (!product) {
+    return { title: 'Product not found' }
+  }
+  return {
+    title: product.name,
+    description: product.description,
+  }
+}
+
+export default async function ProductDetails({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await productService.getBySlug(params.slug)
   if (!product) {
     return <div>Product not found</div>
   }
@@ -63,8 +80,16 @@ export default function ProductDetails({
               </div>
               {product.countInStock !== 0 && (
                 <div className="card-actions justify-center">
-                  <AddToCart
+                  {/* <AddToCart
                     item={{ ...product, qty: 0, color: '', size: '' }}
+                  /> */}
+                  <AddToCart
+                    item={{
+                      ...convertDocToObj(product),
+                      qty: 0,
+                      color: '',
+                      size: '',
+                    }}
                   />
                 </div>
               )}
